5a054c0dab2774a32bb002e90ce4822b27480ffe
Theresa Hemberger <theresahemberger@Theresas-MacBook-Pro17-5.local>
2020-04-07 10:52:02 -0400

    jQuery A HW

---
+++ Homework/jQueryHW/js/jQueryHW.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+  $(".content div:first-child").hide();
+  $(".content div:nth-child(2)").hide();
+  $(".content div:nth-child(3)").hide();
+
+  $("footer").mouseover(function(){
+      $("footer p").html("Email:theresahemberger@gmail.com Phone:123-456-7890" );
+      $("footer p").css("color", "rgb(245, 174, 233)");
+
+  });
+
+  $("footer").mouseleave(function() {
+    $("footer p").html("Contact Info");
+  });
+
+  $("#ingredients").click(function(){
+    $(".content div:first-child").show();
+    $(".content div:nth-child(2)").hide();
+    $(".content div:nth-child(3)").hide();
+  });
+
+  $("#instructions").click(function(){
+    $(".content div:first-child").hide();
+    $(".content div:nth-child(2)").show();
+    $(".content div:nth-child(3)").hide();
+  });
+
+  $("#tips").click(function(){
+    $(".content div:first-child").hide();
+    $(".content div:nth-child(2)").hide();
+    $(".content div:nth-child(3)").show();
+  });
+
+
+});
